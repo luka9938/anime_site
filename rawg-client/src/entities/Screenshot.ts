62737fe85a75c1bb59898907b83c91e6b5d3a6c9
Lukas G <luka9938@stud.kea.dk>
2024-12-05 11:42:11 +0100

    cleaning up

--- rawg-client/src/entities/Screenshot.ts
+++
@@ -1,7 +0,0 @@
-export interface Screenshot {
-  id: number;
-  image: string;
-  width: number;
-  height: number;
-  is_deleted: boolean;
-}
--- rawg-client/src/services/image-url.ts
+++
@@ -1,7 +0,0 @@
-import noImagePlaceholder from "../assets/placeholder.webp";
-const getCroppedImageUrl = (url: string | null) => {
-  if (!url) return noImagePlaceholder;
-  return url.replace("media/games", "media/crop/600/400/games");
-};
-
-export default getCroppedImageUrl;
